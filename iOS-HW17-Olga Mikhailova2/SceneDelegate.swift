d19fa12323d837a8ea664180adbcef7bc6e50861
Olga Mikhailova <foxx_fire@mail.ru>
2025-06-13 21:42:53 +0300

    Delete main

--- iOS-HW17-Olga Mikhailova2/SceneDelegate.swift
+++ iOS-HW17-Olga Mikhailova2/SceneDelegate.swift
@@ -7,7 +7,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: scene)
+        window.rootViewController = ViewController()
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
